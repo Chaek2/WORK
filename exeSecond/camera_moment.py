d6e09b598da752929da3a9383f402b7d804dcd78
Toni <loggerhard@yandex.ru>
2023-11-13 18:12:08 +0300

    14

--- exeSecond/camera_moment.py
+++ exeSecond/camera_moment.py
@@ -3,26 +3,32 @@ import cv2 as cv
 num_cam=1
 
 def img():
-    video=cv.VideoCapture(num_cam)
-    hasFrame,frame=video.read()
-    cv.imwrite("DOWN.jpg", frame)  
+    try:
+        video=cv.VideoCapture(num_cam)
+        hasFrame,frame=video.read()
+        cv.imwrite("DOWN.jpg", frame)  
+    except:
+        pass
 
 def test():
-    video=cv.VideoCapture(num_cam)
-    while 1:
-        hasFrame,frame=video.read()
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-        height, width = gray.shape[:2]
-        start_p_v=(0,int(height/2))
-        end_p_v=(width,int(height/2))
-        start_p_h=(int(width/2),0)
-        end_p_h=(int(width/2),height)
-        circ = (int(width/2),int(height/2))
-        cv.line(gray,start_p_v,end_p_v,(0,255,0),2)
-        cv.line(gray,start_p_h,end_p_h,(0,255,0),2)
-        cv.circle(gray,circ,3,(255,0,0),2)
-        cv.imshow("Display window", gray)
-        if cv.waitKey(1) == ord("q"):
-            break
+    try:
+        video=cv.VideoCapture(num_cam)
+        while 1:
+            hasFrame,frame=video.read()
+            gray = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+            height, width = gray.shape[:2]
+            start_p_v=(0,int(height/2))
+            end_p_v=(width,int(height/2))
+            start_p_h=(int(width/2),0)
+            end_p_h=(int(width/2),height)
+            circ = (int(width/2),int(height/2))
+            cv.line(gray,start_p_v,end_p_v,(0,255,0),2)
+            cv.line(gray,start_p_h,end_p_h,(0,255,0),2)
+            cv.circle(gray,circ,3,(255,0,0),2)
+            cv.imshow("Display window", gray)
+            if cv.waitKey(1) == ord("q"):
+                break
+    except:
+        pass
 
 test()
